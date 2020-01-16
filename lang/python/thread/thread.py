0a1f27600bd7146a0b8e23d2b79363891bd4773c
aries <aries.naa@gmail.com>
2020-01-16 16:57:10 +0500

    + комментарии.

--- lang/python/thread/thread.py
+++ lang/python/thread/thread.py
@@ -8,7 +8,10 @@ stop = threading.Event()
 task_list = threading.Lock()
 
 
-class Rtask:
+# Список потоков, ожидающих выполнения.
+th_list = []
+
+class Rtask():
 
         def __init__(self, name, timeout):
                 self.result = 0
@@ -22,71 +25,105 @@ class Rtask:
                 self.thread.start()
 
         def run(self):
-                print("Running :", self.name,)
+                # Выполняем работу ...
+                print("Running :", self.name, " at ", int(time.time()))
                 self.time = time.time() + self.timeout
 
-                global th_list, stop, task_list
+                # Вернули поток в список ожидания.
                 task_list.acquire()
                 th_list.append(self)
-                stop.set()
                 task_list.release()
+
+                # Сигнао об окончании работы потока.
+                stop.set()
 
         def join(self):
                 if(self.thread != None):
                         self.thread.join()
 
-        def isAlive(self):
-                if(self.thread != None):
-                        return self.thread.isAlive()
-                return 0
 
 
-th_list=[Rtask("thread 1", 10), Rtask("  thread 2", 1), Rtask("    thread 3", 2), Rtask(" thread 4", 5)]
+th_list.extend([
+        Rtask("thread 1    ", 10),
+        Rtask("  thread 2  ", 1),
+        Rtask("    thread 3", 2),
+        Rtask(" thread 4   ", 5)
+])
 
 
-class WThread(threading.Thread):
+#class WThread(threading.Thread):
+#
+#        def __init__(self):
+#                self.result = 0
+#                super().__init__(None, None, None, [])
+#
+#        def run(self):
+#
+#                global th_list, stop, task_list, finish
+#
+#                while(True):
+#                        if(finish.isSet()):
+#                                break
+#                        ctime = time.time()
+#                        task_list.acquire()
+#                        next_time = None
+#                        new_list = []
+#                        for task in th_list:
+#                                if(task.time <= ctime):
+#                                        task.start()
+#                                else:
+#                                        new_list.append(task)
+#                                        if(next_time==None or next_time>task.time):
+#                                                next_time=task.time
+#                        th_list=new_list
+#                        task_list.release()
+#                        if(next_time==None):
+#                                stop.wait()
+#                        else:
+#                                stop.wait(next_time-ctime)
+#
+#                        stop.clear()
+#
+#
+#
+#w=WThread()
+#w.start()
+#
+#time.sleep(60)
+#finish.set()
+#
+#w.join()
+#for task in th_list:
+#	task.join()
 
-        def __init__(self):
-                self.result = 0
-                super().__init__(None, None, None, [])
 
-        def run(self):
+while(True):
 
-                global th_list, stop, task_list, finish
+        ctime = time.time()
 
-                while(True):
-                        if(finish.isSet()):
-                                break
-                        ctime = time.time()
-                        task_list.acquire()
-                        next_time = None
-                        new_list = []
-                        for task in th_list:
-                                if(task.time <= ctime):
-                                        task.start()
-                                else:
-                                        new_list.append(task)
-                                        if(next_time==None or next_time>task.time):
-                                                next_time=task.time
-                        th_list=new_list
-                        task_list.release()
-                        if(next_time==None):
-                                stop.wait()
-                        else:
-                                stop.wait(next_time-ctime)
+        # блокируем список задач для запуска.
+        task_list.acquire()
+        next_time = None
+        new_list = []
 
-                        stop.clear()
+        for task in th_list:
+                # Пришло время запуска
+                if(task.time <= ctime):
+                        task.start()
+                # Ещё не время - выясняем сколько осталось.
+                else:
+                        new_list.append(task)
+                        if(next_time == None or next_time > task.time):
+                                next_time = task.time
 
+        # Новый список задач.
+        th_list = new_list
+        task_list.release()
 
-
-w=WThread()
-w.start()
-
-time.sleep(60)
-finish.set()
-
-w.join()
-for task in th_list:
-	task.join()
-
-
+        # Ждём, когда закончит работу какой либо поток или
+        # подойдёт время запуска потока.
+        if(next_time == None):
+                stop.wait()
+        else:
+                stop.wait(next_time-ctime)
+        stop.clear()
